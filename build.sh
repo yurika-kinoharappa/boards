6511dd544ce46ec94775396164c9259ba3e58a57
Katsuhide Tsuruta <katsuhide.tsuruta@gmail.com>
2025-01-16 02:33:06 +0900

    w

--- build.sh
+++ build.sh
@@ -4,10 +4,8 @@
 curl -sSL https://install.python-poetry.org | python3 -
 # pip3 install poetry
 
-# PATHを設定
-export PATH=$PATH:$HOME/.local/bin
 
 # requirements.txt の生成
-poetry install
+PATH=$PATH:$HOME/.local/bin poetry install
 echo "----export requirements.txt----"
-poetry export -f requirements.txt --output requirements.txt --without-hashes
+PATH=$PATH:$HOME/.local/bin poetry export -f requirements.txt --output requirements.txt --without-hashes
