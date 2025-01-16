ef9e0ca7361b21263dfd9c7b54fe15bb33c663fc
Katsuhide Tsuruta <katsuhide.tsuruta@gmail.com>
2025-01-16 09:05:06 +0900

    w

--- apps/settings.py
+++ apps/settings.py
@@ -76,6 +76,10 @@ WSGI_APPLICATION = "apps.wsgi.app"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
     # "default": {
     #     "ENGINE": "django.db.backends.mysql",
     #     "NAME": "events",
